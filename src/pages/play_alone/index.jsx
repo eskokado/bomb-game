127ceb616fdcc9c5e65c7478f282951d9c4710ae
eskokado <eskokado@gmail.com>
2024-05-25 15:17:57 -0300

    feat(PlayAlone): page

---
+++ src/pages/play_alone/index.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import {
+  Container,
+  TextTimer,
+  Timer,
+  TipContainer,
+  TipText,
+  TipTitle,
+  Title
+} from './styles'
+import bombImg from '../../assets/bomba.png'
+import { ImageBackground } from 'react-native'
+import PasswordInput from '../../components/PasswordInput'
+import { Button } from '../../components/buttons/styles'
+
+function handleNavToStart() {
+  navigation.navigate('Start')
+}
+
+function handleStartGame() {
+  Alert.alert('Jogo começou!')
+}
+
+export default function PlayAlone() {
+  return (
+    <Container>
+      <Title>Bomb Game Solo</Title>
+      <ImageBackground
+        source={bombImg}
+        resizeMode='cover'
+        style={{
+          marginTop: 50,
+          minHeight: 130,
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Timer>
+          <TextTimer>
+            {'00'} : {'05'} : {'00'}
+          </TextTimer>
+        </Timer>
+      </ImageBackground>
+
+      <TipContainer>
+        <TipTitle>Sua dica:</TipTitle>
+        <TipText>Aqui a sua dica</TipText>
+      </TipContainer>
+
+      <PasswordInput />
+
+      <Button buttonText='Iniciar' handlePress={handleStartGame} />
+
+      <Button buttonText='Página Inicial' handlePress={handleNavToStart} />
+    </Container>
+  )
+}
