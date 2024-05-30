293bfdbafb4ccdaeab35bd124f3f1cd2ef8f1c47
eskokado <eskokado@gmail.com>
2024-05-30 11:36:33 -0300

    fix: input password

--- src/components/play_together/input_password/index.jsx
+++ src/components/play_together/input_password/index.jsx
@@ -2,10 +2,20 @@ import React, { useRef } from 'react'
 import { Container, Input, InputContainer } from './style'
 import { Keyboard } from 'react-native'
 
-export default function InputPassword() {
+export default function InputPassword({ pin, setPin, started }) {
   const input1 = useRef()
   const input2 = useRef()
   const input3 = useRef()
+
+  const updatePin = () => {
+    const pinValue = [
+      input1.current.value,
+      input2.current.value,
+      input3.current.value
+    ]
+    setPin(pinValue)
+    console.log('updatePin', pin)
+  }
 
   return (
     <Container>
@@ -15,7 +25,9 @@ export default function InputPassword() {
           maxLength={1}
           ref={input1}
           onChangeText={(value) => {
+            input1.current.value = value
             value && input2.current.focus()
+            updatePin()
           }}
         />
       </InputContainer>
@@ -25,7 +37,9 @@ export default function InputPassword() {
           maxLength={1}
           ref={input2}
           onChangeText={(value) => {
+            input2.current.value = value
             value && input3.current.focus()
+            updatePin()
           }}
         />
       </InputContainer>
@@ -35,6 +49,8 @@ export default function InputPassword() {
           maxLength={1}
           ref={input3}
           onChangeText={(value) => {
+            input3.current.value = value
+            updatePin()
             Keyboard.dismiss()
           }}
         />
