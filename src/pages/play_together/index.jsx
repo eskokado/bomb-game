275f17d63e14e1413a2ef1989ef35b7edc132890
eskokado <eskokado@gmail.com>
2024-05-30 11:37:21 -0300

    feat: impl play together

--- src/pages/play_together/index.jsx
+++ src/pages/play_together/index.jsx
@@ -1,53 +1,97 @@
-import React from 'react'
-import {
-  BombMessage,
-  Container,
-  Input,
-  InputContainer,
-  TextTimer,
-  Timer,
-  Title
-} from './style'
+import React, { useEffect, useState } from 'react'
+import { BombMessage, Container, Title } from './style'
 import InputTimer from '../../components/play_together/input_timer'
 import TipInput from '../../components/play_together/tip_input'
 import InputPassword from '../../components/play_together/input_password'
-import { Button } from '../../components/buttons/styles'
 import { useNavigation } from '@react-navigation/native'
 import ButtonComponent from '../../components/buttons'
-import { Alert } from 'react-native'
+import BombService from '../../services/api/bomb_service'
 
 export default function PlayTogether() {
   const navigation = useNavigation()
 
+  const [pin, setPin] = useState(['', '', ''])
+  const [started, setStarted] = useState(false)
+  const [hours, setHours] = useState('00')
+  const [minutes, setMinutes] = useState('03')
+  const [seconds, setSeconds] = useState('00')
+  const [message, setMessage] = useState('')
+  const [question, setQuestion] = useState('')
+  const [answer, setAnswer] = useState('')
+  const [intervalId, setIntervalId] = useState()
   function handleNavToStart() {
     navigation.navigate('Start')
   }
 
+  function handleStartBomb() {
+    const diffTime = BombService.getDiffTime({ hours, seconds, minutes })
+
+    BombService.startCountdown({
+      setSeconds,
+      setMinutes,
+      setHours,
+      setStarted,
+      diffTime,
+      setIntervalId,
+      intervalId,
+      navigation
+    })
+  }
+
   function handleStartGame() {
-    Alert.alert('O jogo começou')
+    BombService.bombActivationTogether({
+      question,
+      pin,
+      hours,
+      minutes,
+      seconds,
+      setMessage,
+      setStarted,
+      setPin,
+      handleStartBomb,
+      answer,
+      setAnswer
+    })
   }
 
-  function handleDisarmed() {
-    navigation.navigate('Disarmed')
+  function handleDisarmBomb() {
+    BombService.bombDisarmTogether({
+      pin,
+      answer,
+      setStarted,
+      intervalId,
+      setPin,
+      setAnswer,
+      navigation
+    })
   }
 
-  function handleExploded() {
-    navigation.navigate('Exploded')
-  }
+  useEffect(() => {
+    if (started) {
+      handleStartBomb()
+    }
+  }, [started])
 
   return (
     <Container>
       <Title>Bomb Game Dupla</Title>
       <InputTimer />
-      <BombMessage>Mensagem de erro temporária!</BombMessage>
-      <TipInput />
-      <InputPassword />
-      <ButtonComponent buttonText={'Iniciar'} handlePress={handleStartGame} />
-      <ButtonComponent
-        buttonText={'Página Inicial'}
-        handlePress={handleNavToStart}
+      {message ? <BombMessage>{message ? message : null}</BombMessage> : null}
+      <TipInput
+        started={started}
+        answer={answer}
+        setAnswer={setAnswer}
+        setQuestion={setQuestion}
       />
-      <ButtonComponent buttonText={'Sucesso'} handlePress={handleDisarmed} />
+      <InputPassword pin={pin} setPin={setPin} />
+      {!started ? (
+        <ButtonComponent buttonText={'Iniciar'} handlePress={handleStartGame} />
+      ) : (
+        <ButtonComponent
+          buttonText={'Desarmar'}
+          handlePress={handleDisarmBomb}
+        />
+      )}
     </Container>
   )
 }
