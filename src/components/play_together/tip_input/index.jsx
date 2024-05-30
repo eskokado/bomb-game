85634efaf099fe0ab853138eb1cc58102061300d
eskokado <eskokado@gmail.com>
2024-05-30 11:36:49 -0300

    fix: tip input

--- src/components/play_together/tip_input/index.jsx
+++ src/components/play_together/tip_input/index.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Container, Input, InputContainer, TipTitle } from './style'
 
-export default function TipInput() {
+export default function TipInput({ setAnswer, setQuestion }) {
+  const input = useRef()
+
+  const updateAnswer = () => {
+    const answerValue = input.current.value
+
+    setQuestion(answerValue)
+    setAnswer(answerValue)
+  }
+
   return (
     <Container>
       <TipTitle>Dica de senha:</TipTitle>
       <InputContainer>
-        <Input placeholder='Dica para a sua dupla' />
+        <Input
+          placeholder='Dica para a sua dupla'
+          ref={input}
+          onChangeText={(value) => {
+            input.current.value = value
+            updateAnswer()
+          }}
+        />
       </InputContainer>
     </Container>
   )
